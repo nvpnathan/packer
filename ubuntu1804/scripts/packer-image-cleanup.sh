88b349c14f27d31d14f58e93b74d33b6be5790cf
nvpnathan <nathan79@gmail.com>
2019-10-20 21:20:14 -0600

    ubuntu1804 packer template

---
+++ ubuntu1804/scripts/packer-image-cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get autoremove -y
+apt-get clean
+
+# Zero out the free space to save space in the final image:
+echo "Zeroing device to make space..."
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+echo "The End!!!"
+exit 0
