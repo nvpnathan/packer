e355479277101a5a4609c5d9b49839dd611b9814
nvpnathan <nathan79@gmail.com>
2020-05-06 14:03:37 -0600

    added 2004

---
+++ ubuntu2004/scripts/packer-install-base.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get clean
+apt -y update
+apt -y upgrade
+
+# Update to the latest kernel
+apt-get install -y linux-generic linux-image-generic
+
+# Hide Ubuntu splash screen during OS Boot, so you can see if the boot hangs
+apt-get remove -y plymouth-theme-ubuntu-text
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
+update-grub
+
+# Install base packages
+apt-get install -y wget -q  unzip git curl net-tools tar gawk tcpdump netcat sshpass jq vim
+
+# Reboot with the new kernel
+shutdown -r now
+sleep 60
+
+exit 0
+
