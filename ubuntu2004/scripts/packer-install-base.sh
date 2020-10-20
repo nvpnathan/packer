63017a584606bf76e6a2ba0b38a705d39d05f8a9
nvpnathan <nathan79@gmail.com>
2020-10-19 20:27:31 -0600

    updated packer

--- ubuntu2004/scripts/packer-install-base.sh
+++ ubuntu2004/scripts/packer-install-base.sh
@@ -15,6 +15,35 @@ update-grub
 # Install base packages
 apt-get install -y wget -q  unzip git curl net-tools tar gawk tcpdump netcat sshpass jq vim
 
+# install docker
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+# use docker as non-root
+sudo groupadd docker
+sudo usermod -aG docker nathan
+sudo systemctl enable docker
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# install terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
 # Reboot with the new kernel
 shutdown -r now
 sleep 60
